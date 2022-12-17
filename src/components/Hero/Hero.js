ec01d9ce2e6cf55dd92e6d1e08774d76d099205e
HelloWorldItsMeYourProgrammer <92838200+HelloWorldItsMeYourProgrammer@users.noreply.github.com>
2022-12-17 22:33:44 +0200

    hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { Section, SectionText, SectionTitle } from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          Personal Portfolio Of <br />
+          Karolis Šidlauskas
+        </SectionTitle>
+        <SectionText>
+          I’m a software developer with the MERN Full-Stack development knowledge. Althought I specialise in MongoDB, Express.js, React.js, Node.js
+          tools, I'm always keen to learn new ways of software building.
+        </SectionText>
+        <Button onClick={props.handleClick}>Learn More</Button>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
